824c69eca395e7be78d5a3446b3d12bebfb4b865
DonyMagRony <121221540+DonyMagRony@users.noreply.github.com>
2024-03-22 21:15:08 +0500

    Delete Lab7/infomatics/hackerrannk/company logo.py

--- Lab7/infomatics/hackerrannk/company logo.py
+++
@@ -1,9 +0,0 @@
-
-import sys, collections
-
-if __name__ == "__main__":
-    s = input().strip()
-    count_object = collections.Counter(s)
-    output = sorted(count_object.items(), key=lambda x: (-x[1], x[0]))
-    for i in range(0, 3):
-        print("{} {}".format(output[i][0], output[i][1]))
